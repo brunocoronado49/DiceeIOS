dc2343c9789946d3c077900c66f6025c61450267
brunocoronado49 <francorangelcoronado@gmail.com>
2024-05-30 17:34:59 -0600

    terminando app, pero falta ajustarla a los tamanios de pantalla

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewController.swiftq
 //  Dicee
 //
 //  Created by Bruno  on 24/05/24.
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Referencias a elementos de la UI
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    // Este metodo se lanza al iniciar la app
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.alpha = 0.5
+        diceImageViewTwo.alpha = 0.5
     }
 
-
+    // Funcion que cambia las propiedades por medio de un
+    // ItemBuilderAction (accion del usuario - boton)
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewOne.alpha = 1
+        diceImageViewTwo.alpha = 1
+        
+        let diceArray = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")]
+                
+        diceImageViewOne.image = diceArray.randomElement()
+        diceImageViewTwo.image = diceArray.randomElement()
+        
+    }
+    
 }
 
